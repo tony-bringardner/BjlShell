9fea2546d7d8fccf8b3d09452b0ef10f27451772
tony-bringardner <tony.bringardner@gmail.com>
2025-07-26 22:30:52 -0400

    Update SelectStatement.java

--- src/main/java/us/bringardner/shell/antlr/statement/SelectStatement.java
+++ src/main/java/us/bringardner/shell/antlr/statement/SelectStatement.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 
