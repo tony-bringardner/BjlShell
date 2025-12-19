d73a2df3d123d0e80b6c0257ca4f9e6cbf6151af
Tony Bringardner <tony-bringardner@users.noreply.github.com>
2025-12-19 11:13:21 -0500

    Update TestCpRm.java

--- src/test/java/us/bringardner/shell/test/TestCpRm.java
+++ src/test/java/us/bringardner/shell/test/TestCpRm.java
@@ -215,7 +215,7 @@ public class TestCpRm extends AbstractConsoleTest{
 		String err = res.getStdErr();
 		assertEquals(0, res.exitCode,"Rm exit code");
 		assertTrue(out.startsWith("remove"),"First part of the prompt is wrong");
-		if( getOs()==OperatingSystem.Windows) {
+		if( getOs()!=OperatingSystem.Windows) {
 			assertTrue(out.endsWith("CpTestFiles/../target/Co01.txt?"),"Last part of the prompt is wrong");
 		} else {
 			assertTrue(out.endsWith("CpTestFiles\\..\\target\\Co01.txt?"));
@@ -228,8 +228,11 @@ public class TestCpRm extends AbstractConsoleTest{
 		err = res.getStdErr();
 		assertEquals(0, res.exitCode,"Rm exit code");		
 		assertTrue(out.startsWith("remove"),"First part of the prompt is wrong");
-		assertTrue(out.endsWith("CpTestFiles/../target/Co01.txt?"),"Last part of the prompt is wrong");
-		
+		if( getOs()!=OperatingSystem.Windows) {
+			assertTrue(out.endsWith("CpTestFiles/../target/Co01.txt?"),"Last part of the prompt is wrong");
+		} else {
+			assertTrue(out.endsWith("CpTestFiles\\..\\target\\Co01.txt?"));
+		}
 		assertEquals("", err,"Err should be empty");
 		assertEquals(0, res.exitCode,"Rm exit code");		
 		assertFalse(file.exists(),"File still exist after rm");
