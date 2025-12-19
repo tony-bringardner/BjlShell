fd2ab4dc212291b9c95e606b4e273da3c1dd700a
Tony Bringardner <tony-bringardner@users.noreply.github.com>
2025-12-19 06:06:31 -0500

    Update TestVirtualFileSystem.java

--- src/test/java/us/bringardner/shell/test/TestVirtualFileSystem.java
+++ src/test/java/us/bringardner/shell/test/TestVirtualFileSystem.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -33,17 +32,6 @@ public class TestVirtualFileSystem extends AbstractConsoleTest{
 		//System.out.println("After all");
 	}
 
-	private static final String FILE_NAME_PATTERN = "Test-%d-%d.txt";
-
-
-	private static void fillDir(FileSource dir,int i) throws IOException {
-		for(int idx=0; idx < 10; idx++ ) {
-			FileSource file = dir.getChild(String.format(FILE_NAME_PATTERN, i,idx));
-			try(OutputStream out = file.getOutputStream()) {
-				out.write("0123456789".getBytes());
-			}
-		}		
-	}
 
 	@Test
 	@Order(2)
@@ -94,19 +82,19 @@ public class TestVirtualFileSystem extends AbstractConsoleTest{
 
 		FileSource[] kids = tmp.listFiles();
 		String expectFiles [] = (
-				"/AbcFile.js\n"
-						+ "/AbcFile.php\n"
-						+ "/AbcFile.properties\n"
-						+ "/Folder01\n"
-						+ "/Hotel California.txt\n"
-						+ "/SymLink2Folder01").split("\n");
+				"AbcFile.js\n"
+						+ "AbcFile.php\n"
+						+ "AbcFile.properties\n"
+						+ "Folder01\n"
+						+ "Hotel California.txt\n"
+						+ "SymLink2Folder01").split("\n");
 		assertEquals(expectFiles.length, kids.length);
 
 		for (int idx = 0; idx < expectFiles.length; idx++) {
 			FileSource kid = kids[idx];
 			path = kid.getAbsolutePath();
 			expect= expectFiles[idx];
-			assertEquals(expect, path);			
+			assertTrue(path.endsWith(expect));			
 		}
 
 		if( getOs()==OperatingSystem.Windows) {
