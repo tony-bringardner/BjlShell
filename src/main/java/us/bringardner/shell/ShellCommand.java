3edba7748d464a2ee41b934656144e23f1a19d8d
Tony Bringardner <tony-bringardner@users.noreply.github.com>
2025-12-28 16:24:02 +0000

    Update ShellCommand.java

--- src/main/java/us/bringardner/shell/ShellCommand.java
+++ src/main/java/us/bringardner/shell/ShellCommand.java
@@ -6,8 +6,6 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -375,15 +373,6 @@ public abstract class ShellCommand {
 			ret = glob(ctx, path);
 		} else {
 			ret.add(ctx.console.createFileSource(path));
-		}
-		if( !ret.isEmpty()) {
-			Collections.sort(ret,new Comparator<FileSource>() {
-
-				@Override
-				public int compare(FileSource o1, FileSource o2) {
-					return o1.getName().compareTo(o2.getName());
-				}
-			});
 		}
 
 		return ret;
